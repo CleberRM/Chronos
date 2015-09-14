0b8589b2d05eeaec107636b8972b9899c39fb6f9
CleberRM <c.moschini@globalsolution.com.br>
2015-09-14 19:42:11 -0300

    Inicialização do projeto Chronus

---
+++ br.com.Chronos/br.com.Chronos.Negocio/IAcoesBanco.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace br.com.Chronos.Negocio
+{
+    public interface IAcoesBanco<T>
+    {
+        /// <summary>
+        /// Retorna a entidade correspondente ao id passado
+        /// </summary>
+        /// <param name="id">Id da entidade</param>
+        /// <returns>Entidade que atenda ao parametro.</returns>
+        T RetornarEntidadePor(int id);
+
+        /// <summary>
+        /// Salva a entidade passada
+        /// </summary>
+        /// <param name="entidade">Entidade a ser salva</param>
+        /// <returns>Código da entidade salva no banco de dados.</returns>
+        int Salvar(T entidade);
+
+        /// <summary>
+        /// Retorna lista das entidades que atendam ao filtro passado
+        /// </summary>
+        /// <param name="entidade">Entidade que deve ser preenchida com os dados utilizados para filtrar a consulta. Passar parametro nulo para pesquisa sem filtro.</param>
+        /// <returns>Lista contendo todas as entidades que atendam ao filtro passado. No caso de parametro nulo, retorna todas as entidades da tabela.</returns>
+        List<T> RetornarLista(T entidade);
+
+        /// <summary>
+        /// Excluir entidade correspondente ao id passado
+        /// </summary>
+        /// <param name="id">Id da entidade</param>
+        /// <returns>Booleano indicando se a exclusão foi executada com sucesso.</returns>
+        bool ExcluirEntidadePor(int id);
+    }
+}
