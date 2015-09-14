05033aea19f73674b31901fe9c262b71a7f864d5
CleberRM <c.moschini@globalsolution.com.br>
2015-09-14 20:09:54 -0300

    Criacao da classe RepositorioFake

---
+++ br.com.Chronos/br.com.Chronos.FakeRepositorio/RepositorioFake.cs
@@ -0,0 +1,32 @@
+﻿using br.com.Chronos.Negocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace br.com.Chronos.FakeRepositorio
+{
+    public class RepositorioFake<T> : IAcoesBanco<T>
+    {
+        public bool ExcluirEntidadePor(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T RetornarEntidadePor(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> RetornarLista(T entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Salvar(T entidade)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
