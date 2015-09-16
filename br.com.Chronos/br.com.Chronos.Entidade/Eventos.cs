6fdb8eb4e30f39dd1926dc79e2a3984e1732ea36
afvsouza <a.fvsouza@gmail.com>
2015-09-16 11:53:48 -0300

    Criação da classe Eventos

---
+++ br.com.Chronos/br.com.Chronos.Entidade/Eventos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace br.com.Chronos.Entidade
+{
+    class Eventos : IEntidade
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public string CorHex { get; set; }
+
+        
+    }
+}
