4d8e96da34c25b574c85e7da57d06e14aabb48fb
henriquecbs <henrique@globalsolution.com.br>
2015-09-17 14:48:07 -0300

    Inserido os atributos na classe cliente

--- br.com.Chronos/br.com.Chronos.Entidade/Cliente.cs
+++ br.com.Chronos/br.com.Chronos.Entidade/Cliente.cs
@@ -6,7 +6,48 @@ using System.Threading.Tasks;
 
 namespace br.com.Chronos.Entidade
 {
-    class Cliente
+    public class Cliente: IEntidade
     {
+        public string NomeReduzido { get; set; }
+        public string NomeCompleto { get; set; }
+        public string CNPJeCPF { get; set; }
+        public string Endereco { get; set; }
+        public int Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string CEP { get; set; }
+        public string Cidade { get; set; }
+        public string UF { get; set; }
+        public string Pais { get; set; }
+        public string NomeVendedor { get; set; }
+        public string Observacoes { get; set; }
+        public string ConexaoRemota { get; set; }
+
+        public int Telefone { get; set; }
+        public int Fax { get; set; }
+        public int VersaoAtual { get; set; }
+        public int QuantidadeLicencas { get; set; }
+        public int TipoCliente { get; set; }
+
+        public bool ProdutoAcess { get; set; }
+        public bool ProdutoOfficecomexMax { get; set; }
+        public bool ProdutoOfficecomexInternacional { get; set; }
+        public bool ProdutoFollowebInternacional { get; set; }
+        public bool ProdutoFolloweb { get; set; }
+        public bool Ativo { get; set; }
+        public bool ClienteProspecto { get; set; }
+
+        int IEntidade.Id
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
