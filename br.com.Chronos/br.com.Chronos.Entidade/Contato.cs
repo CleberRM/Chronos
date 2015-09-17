4db6250b11b757450a11522435eab0f47f89ebe3
RISADASK8 <risada.ricardo@gmail.com>
2015-09-17 00:38:51 -0300

    incluso a classe Contato

---
+++ br.com.Chronos/br.com.Chronos.Entidade/Contato.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace br.com.Chronos.Entidade
+{
+     public class Contato : IEntidade
+
+    {
+        public int Id { get; set; }
+        
+        public string nomeContato { get; set; }
+
+        public string telefoneContato { get; set; }
+
+        public string celularContato { get; set; }
+
+        public string setorContato { get; set; }
+
+        public string ramalContato { get; set; }
+
+        public string skypeContato { get; set; }
+
+        public string emailContato { get; set; }
+
+        public DateTime dataniverContato { get; set; }
+
+        public DateTime dataRegistroContato { get; set; }
+
+        public string respRegistroContato { get; set; }
+
+        public bool treinamentoContato { get; set; }
+
+    }
+}
