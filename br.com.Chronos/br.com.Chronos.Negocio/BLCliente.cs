9a2860d94767f922d9b52e6e4598e3ccc4ca177f
henriquecbs <henrique@globalsolution.com.br>
2015-09-20 20:49:38 -0300

    Adicionando BLCliente

---
+++ br.com.Chronos/br.com.Chronos.Negocio/BLCliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using br.com.Chronos.Entidade;
+namespace br.com.Chronos.Negocio
+{
+         public class BLCliente : ANegocio<Cliente>
+        {
+            public BLCliente(IAcoesBanco<Cliente> acoesBanco) : base(acoesBanco)
+            {
+
+            }
+        }
+}
