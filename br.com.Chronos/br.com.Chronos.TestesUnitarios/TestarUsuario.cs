6d25e3294d1c6bdff97ec9c29e062508979090e3
gainfiorotti <gianfiorotti@gmail.com>
2015-09-21 19:30:08 -0300

    Inserindo Classe TestarClasseUsuario

--- br.com.Chronos/br.com.Chronos.TestesUnitarios/TestarUsuario.cs
+++ br.com.Chronos/br.com.Chronos.TestesUnitarios/TestarUsuario.cs
@@ -5,17 +5,37 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using br.com.Chronos.Entidade;
+using br.com.Chronos.Negocio;
+using br.com.Chronos.FakeRepositorio;
 
 namespace br.com.Chronos.TestesUnitarios
 {
     [TestClass]
-    class TestarUsuario
+    public class TestarUsuario
     {
 
         [TestMethod]
         public void TestarClasseUsuario()
         {
+
+            Setor setor = new Setor();
+            setor.Descricao = "Projetos";
+
+            Escritorio office = new Escritorio();
+            office.NomeEscritorio = "Sao";
+
+            Usuario user = new Usuario("Gian", "1",office, setor);
+
+            IAcoesBanco<Usuario> acoesBanco = new RepositorioFake<Usuario>();
+
+            BLUsuario blUsuario = new BLUsuario(acoesBanco);
+
+            blUsuario.Salvar(user);
+
+            Assert.AreEqual("Gian", user.NomeUsuario);
             
+                
+
         }
 
 
