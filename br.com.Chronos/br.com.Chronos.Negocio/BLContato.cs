a74d039a9a97e50f6b966679cc43e0b3c5712306
RISADASK8 <risada.ricardo@gmail.com>
2015-09-21 13:12:15 -0300

    Incluso BLContato ao projeto

---
+++ br.com.Chronos/br.com.Chronos.Negocio/BLContato.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using br.com.Chronos.Entidade;
+
+namespace br.com.Chronos.Negocio
+{
+    public class BLContato : ANegocio<Contato>
+    {
+        public BLContato(IAcoesBanco<Contato> acoesBanco) : base(acoesBanco)
+        {
+
+        }
+    }
+}
