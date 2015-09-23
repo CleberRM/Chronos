41c827ad87734fa03b9f0f93d5b547156d1c9e0d
GuiMassi <guilhermemassi@outlook.com>
2015-09-23 13:37:41 -0300

    Alterado nome da classe AdicionarEvento para LancamentoEvento

--- br.com.Chronos/br.com.Chronos.Entidade/LancamentoEvento.cs
+++ br.com.Chronos/br.com.Chronos.Entidade/LancamentoEvento.cs
@@ -6,22 +6,19 @@ using System.Threading.Tasks;
 
 namespace br.com.Chronos.Entidade
 {
-    public class AdicionarEvento
+    public class LancamentoEvento : AEntidade
     {
-        public AdicionarEvento(Eventos evento)
+        public LancamentoEvento(Eventos evento)
         {
             EventoLancado = evento;
-            DataCriacao = DateTime.Now;
         }
 
-        public int CodigoEvento{ get; set; }
+       
         public Eventos EventoLancado { get; private set; }
-        public Usuario ResponsavelCriacao { get;private set; }
         public Usuario ResponsavelEvento { get; set; }
-        public DateTime DataCriacao { get; set; }
         public DateTime DataPrevistaConclusao { get; set; }
         public DateTime DataConclusao { get; set; }
-        public String StatusEvento { get; set; }
+        public string StatusEvento { get; set; }
 
     }
 
