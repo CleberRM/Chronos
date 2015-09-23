dadb4b9f26cdd12c07c45f4dee5c65b0aceb73fc
GuiMassi <guilhermemassi@outlook.com>
2015-09-23 11:43:05 -0300

    Criado AdicionarEvento

---
+++ br.com.Chronos/br.com.Chronos.Entidade/AdicionarEvento.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace br.com.Chronos.Entidade
+{
+    public class AdicionarEvento
+    {
+        public AdicionarEvento(Eventos evento)
+        {
+            EventoLancado = evento;
+            DataCriacao = DateTime.Now;
+        }
+
+        public int CodigoEvento{ get; set; }
+        public Eventos EventoLancado { get; private set; }
+        public Usuario ResponsavelCriacao { get;private set; }
+        public Usuario ResponsavelEvento { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public DateTime DataPrevistaConclusao { get; set; }
+        public DateTime DataConclusao { get; set; }
+        public String StatusEvento { get; set; }
+
+    }
+
+}
