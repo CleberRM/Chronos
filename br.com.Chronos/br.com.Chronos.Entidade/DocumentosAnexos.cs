45f69c54e4903403b16384ecc8c30bfc310227d2
GuiMassi <guilhermemassi@outlook.com>
2015-09-23 16:57:28 -0300

    Adiconado Novas Classes -Modalidade -DocumentosAnexos -MotivoOS

---
+++ br.com.Chronos/br.com.Chronos.Entidade/DocumentosAnexos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace br.com.Chronos.Entidade
+{
+    public class DocumentosAnexos : AEntidade
+    {
+        public string NomeDocumento {get; set;}
+        public string CaminhoDocumento { get; set; }
+ 
+    }
+
+}
