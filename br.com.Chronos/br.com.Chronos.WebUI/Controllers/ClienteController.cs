32708525b7475bfe1d562d766c20611a9a045a58
henriquecbs <henrique@globalsolution.com.br>
2015-10-06 11:21:27 -0300

    inserido a pagina de cliente

---
+++ br.com.Chronos/br.com.Chronos.WebUI/Controllers/ClienteController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using br.com.Chronos.Negocio;
+using br.com.Chronos.Entidade;
+
+namespace br.com.Chronos.WebUI.Controllers
+{
+    public class ClienteController : Controller
+    {
+        private ANegocio<Cliente> _blCliente;
+        public ClienteController(ANegocio<Cliente> blCliente)
+        {
+            this._blCliente = blCliente;
+        } 
+
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
